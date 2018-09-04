a76375dd3826f08eeee132e3a4699361ba4aaee6
balatamilmani <btamilma.ptr@gmail.com>
2018-09-04 01:48:53 -0400

    Sample Node js Rest service

---
+++ rest-service/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( "./users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
